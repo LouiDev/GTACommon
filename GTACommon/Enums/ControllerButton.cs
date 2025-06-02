04a460721d69d456ed215f04e3daf392ae4ec729
LouiDev <2705loui@gmail.com>
2025-06-02 17:49:22 +0200

    feat: add ControllerButton enum

---
+++ GTACommon/Enums/ControllerButton.cs
@@ -0,0 +1,18 @@
+﻿namespace GTACommon.Enums
+{
+    public enum ControllerButton
+    {
+        PadA = 18,
+        PadB = 57,
+        PadX = 22,
+        PadY = 23,
+        PadRight = 14,
+        PadLeft = 15,
+        PadDown = 19,
+        PadUp = 27,
+        PadLB = 37,
+        PadRB = 44,
+        PadLT = 25,
+        PadRT = 24
+    }
+}
