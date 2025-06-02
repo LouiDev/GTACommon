0bd9e03b09c233303d027dd07ddfa03fabf6609c
LouiDev <2705loui@gmail.com>
2025-06-02 17:56:28 +0200

    feat: add ControllerButtonExtensions class

---
+++ GTACommon/Extensions/ControllerButtonExtensions.cs
@@ -0,0 +1,10 @@
+﻿using GTA.Native;
+using GTACommon.Enums;
+
+namespace GTACommon.Extensions
+{
+    public static class ControllerButtonExtensions
+    {
+        public static string GetInstructionalId(this ControllerButton button) => Function.Call<string>(Hash.GET_CONTROL_INSTRUCTIONAL_BUTTONS_STRING, 2, (int)button, true);
+    }
+}
