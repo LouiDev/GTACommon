c1149fb6fc68dcccd898cc020187f80a7e2117e2
LouiDev <2705loui@gmail.com>
2025-06-02 17:54:56 +0200

    feat: add ControlExtensions class

---
+++ GTACommon/Extensions/ControlExtensions.cs
@@ -0,0 +1,10 @@
+﻿using GTA;
+using GTA.Native;
+
+namespace GTACommon.Extensions
+{
+    public static class ControlExtensions
+    {
+        public static string GetInstructionalId(this Control control) => Function.Call<string>(Hash.GET_CONTROL_INSTRUCTIONAL_BUTTONS_STRING, 2, (int)control, true);
+    }
+}
