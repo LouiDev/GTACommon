656c36aa1217a1639449e36b275a72d07bb2cac7
LouiDev <2705loui@gmail.com>
2025-06-03 09:59:17 +0200

    feat: add Common class

---
+++ GTACommon/Common.cs
@@ -0,0 +1,22 @@
+﻿using GTA.Native;
+
+namespace GTACommon
+{
+    public class Common
+    {
+        /// <summary>
+        /// Starts a busy spinner with the specified text
+        /// </summary>
+        public static void StartSpinner(string text)
+        {
+            Function.Call(Hash.BEGIN_TEXT_COMMAND_BUSYSPINNER_ON, "STRING");
+            Function.Call(Hash.ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME, text);
+            Function.Call(Hash.END_TEXT_COMMAND_BUSYSPINNER_ON, 3);
+        }
+
+        /// <summary>
+        /// Ends the current busy spinner
+        /// </summary>
+        public static void EndSpinner() => Function.Call(Hash.BUSYSPINNER_OFF);
+    }
+}
